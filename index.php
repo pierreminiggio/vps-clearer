8c0c8bea177efcb6d0dc6b83ad544cf725cf6f4d
Pierre MINIGGIO <pierre.miniggio@gmail.com>
2021-02-24 23:49:22 +0100

    clear caches

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ function runCommand(string $command): void
     ) . PHP_EOL;
 }
 
+// Clearing RAM
 runCommand('npm --prefix /var/www/cron/boosted-scraper stop');
 
 for ($i = 0; $i < 10; $i++) {
@@ -19,5 +20,10 @@ for ($i = 0; $i < 10; $i++) {
 }
 
 runCommand('npm --prefix /var/www/cron/boosted-scraper start');
+
+// Clearing caches
+runCommand('find /var/www/html/gtts-api/public/cache/ -type f -mtime +7 -name \'*.mp3\' -execdir rm -- \'{}\' \;');
+runCommand('find /var/www/html/youtube-video-random-clip-api/public/cache -type f -mtime +7 -execdir rm -- \'{}\' \;');
+runCommand('find /var/www/html/youtube-video-random-clip-api/public/video -type f -mtime +7 -execdir rm -- \'{}\' \;');
 
 exit;
