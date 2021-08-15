696556f37adea257835e95fd84fa9fc3c4546bd9
root <root@vps-30597b40.vps.ovh.net>
2021-08-15 11:54:40 +0000

    clear old processed files

--- clear.sh
+++ clear.sh
@@ -3,8 +3,8 @@
 pkill chrome
 pkill chrome
 pkill chrome
-pkill chrome
 
 find /var/www/html/gtts-api/public/cache/ -type f -mtime +7 -name '*.mp3' -execdir rm -- '{}' ';'
+find /var/www/html/gtts-api/public/cache/processed/ -type f -mtime +7 -name '*.mp3' -execdir rm -- '{}' ';'
 find /var/www/html/youtube-video-random-clip-api/public/cache -type f -mtime +7 -execdir rm -- '{}' ';'
 find /var/www/html/youtube-video-random-clip-api/public/video -type f -mtime +7 -execdir rm -- '{}' ';'
