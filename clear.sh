36542aed30ce68737fd702925a500aca4135d9c0
root <root@vps-30597b40.vps.ovh.net>
2021-08-30 22:27:01 +0000

    clear speechelo

--- clear.sh
+++ clear.sh
@@ -6,5 +6,6 @@ pkill chrome
 
 find /var/www/html/gtts-api/public/cache/ -type f -mtime +7 -name '*.mp3' -execdir rm -- '{}' ';'
 find /var/www/html/gtts-api/public/cache/processed/ -type f -mtime +7 -name '*.mp3' -execdir rm -- '{}' ';'
+find /var/www/html/gtts-api/public/cache/speechelo/ -type f -mtime +7 -name '*.mp3' -execdir rm -- '{}' ';'
 find /var/www/html/youtube-video-random-clip-api/public/cache -type f -mtime +7 -execdir rm -- '{}' ';'
 find /var/www/html/youtube-video-random-clip-api/public/video -type f -mtime +7 -execdir rm -- '{}' ';'
